5f347952ab3aa24130a31e3f3494506da5f1d2ef
AlecDusheck <a.dusheckirl@gmail.com>
2019-03-12 17:33:20 -0500

    Add controlsService

---
+++ src/services/gameserver/ControlsService.ts
@@ -0,0 +1,54 @@
+import { ActionFailed } from "../../util/errors/ActionFailed";
+import { NodeInterface } from "../../util/NodeInterface";
+
+export class ControlsService {
+  public executeCommand = async (server: any, command: string) => {
+    const nodeInterface = new NodeInterface(server._nodeInstalled);
+    try {
+      await nodeInterface.execute(server, command);
+    } catch (e) {
+      switch (NodeInterface.niceHandle(e)) {
+        case "SERVER_NOT_OFF":
+          throw new ActionFailed("Server not off.", true);
+        default:
+          throw new ActionFailed("Unknown error.", true);
+      }
+    }
+  };
+
+  public install = async (server: any) => {
+    const nodeInterface = new NodeInterface(server._nodeInstalled);
+    try {
+      await nodeInterface.install(server);
+    } catch (e) {
+      switch (NodeInterface.niceHandle(e)) {
+        case "SERVER_LOCKED":
+          throw new ActionFailed("Server is locked.", true);
+        case "REINSTALL_INSTEAD":
+          throw new ActionFailed("Reinstall your server instead.", true);
+        case "SERVER_NOT_OFF":
+          throw new ActionFailed("Server not off.", true);
+        default:
+          throw new ActionFailed("Unknown error.", true);
+      }
+    }
+  };
+
+  public reinstall = async (server: any) => {
+    const nodeInterface = new NodeInterface(server._nodeInstalled);
+    try {
+      await nodeInterface.reinstall(server);
+    } catch (e) {
+      switch (NodeInterface.niceHandle(e)) {
+        case "SERVER_LOCKED":
+          throw new ActionFailed("Server is locked.", true);
+        case "INSTALL_INSTEAD":
+          throw new ActionFailed("Install your server instead.", true);
+        case "SERVER_NOT_OFF":
+          throw new ActionFailed("Server not off.", true);
+        default:
+          throw new ActionFailed("Unknown error.", true);
+      }
+    }
+  };
+}
